0d1425af5f52f8b91e49241bcf0cc6cfcbbf56aa
Vitor Burns <122344541+G0wth@users.noreply.github.com>
2023-06-24 01:30:34 -0300

    quase lá ;-;

--- index.js
+++ index.js
@@ -6,230 +6,175 @@
 - Durante o sorteio dos números, cada cartela deverá ter o número sorteado marcado
 - O nome dos jogadores vencedores deverá aparecer na tela
 */
-/*
-class cardNumber {
-    constructor(number, is_match){
-        this.number = number;
-        this.match = is_match;
+
+var totalPlayers = [];
+var sorted_numbers = [];
+var gameProgress = false;
+
+// função para detrtminar a geração de números aleatórios
+function generateRandomNumbers(amount, min, max) {
+
+  if (amount > (max - min)) {
+    console.log("Intervalo...");
+    return;
+  }
+
+  var number = [];
+
+  while (number.length < amount) {
+    var random = Math.floor(Math.random() * (max - min) + min);
+
+    if (!number.includes(random)) {
+      number.push(random);
     }
+  }
+
+  return number;
 }
 
-let cardNumber = {number: 0, match: false}
-
-const exampleNumber = {number: 0, is_match: false};
-const exampleCard = {name:"Bananatico", cardNumbers: []};
-
-function generateCardbyExample(){
-    let newCard = exampleCard;
-
-
-
-}
-*/
-
-let totalPlayers = [];
-let gameProgress = false;
-
-function generateRandomNumbers(amount, min, max){
-
-    if(amount > (max - min)){
-        console.log("Intervalo...");
-        return;
-    }
-
-    var number = [];
-
-    while(number.length < amount){
-        var random = Math.floor(Math.random()*(max - min) + min);
-
-        if(!number.includes(random)){
-            number.push(random);
-        }
-    }
-
-    return number;
-}
-
+// função para adicionar os valores gerados em cada coluna da cartela do bingo
 function generateCard() {
-    var playerName = prompt('Digite o nome do jogador.') || '';
-  
-    if (playerName.trim().length === 0) {
-      alert('Escreva um nome com pelo menos 1 dígito');
-      return;
-    }
-  
-    if (totalPlayers.some(player => player.name === playerName)) {
-      alert('Esse nome já existe. Escolha um nome diferente.');
-      return;
-    }
-  
-    if (gameProgress == true) {
-      alert("Não é possível criar uma cartela com o jogo em andamento!");
-      return;
-    }
-  
-    var card = [
-      generateRandomNumbers(5, 1, 15),
-      generateRandomNumbers(5, 16, 30),
-      generateRandomNumbers(5, 31, 45),
-      generateRandomNumbers(5, 46, 60),
-      generateRandomNumbers(5, 61, 75)
-    ];
-  
-    while (card.length < 25) {
-      var random_number = Math.floor(Math.random() * 75 + 1);
-      if (!card.includes(random_number)) {
-        card.push(random_number);
-      }
-    }
-  
-    let player = {
-      name: playerName,
-      card: card
-    };
-  
-    totalPlayers.push(player);
-    console.log(totalPlayers);
-  
-    console.log(card);
-    return { playerName, card };
+  var card = [
+    generateRandomNumbers(5, 1, 15),
+    generateRandomNumbers(5, 16, 30),
+    generateRandomNumbers(5, 31, 45),
+    generateRandomNumbers(5, 46, 60),
+    generateRandomNumbers(5, 61, 75)
+  ];
+
+  console.log(card);
+  return card;
+}
+
+// função para gerar a cartela do jogador
+function insertPlayer() {
+  const playerName = prompt('Digite o nome do jogador.');
+
+  if (playerName.length < 1) {
+    alert('Escreva um nome com pelo menos 1 caractere');
+    return;
   }
-  
 
-function drawCard(){
+  const isNameExists = totalPlayers.some(player => player.name === playerName);
 
-    const { playerName, card} = generateCard();
+  if (isNameExists) {
+    alert('Esse nome já existe. Escolha um nome diferente.');
+    return;
+  }
 
-    const div_space_card = document.getElementById('space_card');
+  if (gameProgress == true) {
+    alert("Não é possível criar uma cartela com o jogo em andamento!");
+    return;
+  }
 
-    const div_card = document.createElement('div');
-    div_card.className = 'card';
+  const card = generateCard();
 
-    div_space_card.appendChild(div_card);
+  const player = {
+    name: playerName,
+    card: card
+  }
 
-    const h4_player = document.createElement('h4');
-    h4_player.innerText = playerName;
+  totalPlayers.push(player);
+  console.log(totalPlayers);
+  drawCard(player);
+}
 
-    div_card.appendChild(h4_player)
+// função para construir as cartelas na tela
+function drawCard(player) {
+  generateCard();
 
-    const table_card = document.createElement('table');
-    const thead_card = document.createElement('thead');
-    const tbody_card = document.createElement('tbody');
+  const div_space_card = document.getElementById('space_card');
+
+  const div_card = document.createElement('div');
+  div_card.className = 'card';
+
+  div_space_card.appendChild(div_card);
+
+  const h4_player = document.createElement('h4');
+  h4_player.innerText = player.name;
+
+  div_card.appendChild(h4_player)
+
+  const table_card = document.createElement('table');
+  const thead_card = document.createElement('thead');
+  const tbody_card = document.createElement('tbody');
 
 
-    //tabela do bingo
-    table_card.appendChild(thead_card);
-    table_card.appendChild(tbody_card);
-    div_card.appendChild(table_card)
+  table_card.appendChild(thead_card);
+  table_card.appendChild(tbody_card);
+  div_card.appendChild(table_card)
 
-    //th BINGO 
-    const th_B = document.createElement('th');
-    const th_I = document.createElement('th');
-    const th_N = document.createElement('th');
-    const th_G = document.createElement('th');
-    const th_O = document.createElement('th');
 
-    th_B.innerText = 'B'
-    th_I.innerText = 'I'
-    th_N.innerText = 'N'
-    th_G.innerText = 'G'
-    th_O.innerText = 'O'
+  const th_B = document.createElement('th');
+  const th_I = document.createElement('th');
+  const th_N = document.createElement('th');
+  const th_G = document.createElement('th');
+  const th_O = document.createElement('th');
 
-    thead_card.appendChild(th_B);
-    thead_card.appendChild(th_I);
-    thead_card.appendChild(th_N);
-    thead_card.appendChild(th_G);
-    thead_card.appendChild(th_O);
 
-    for(var n = 0; n < 5; n++){
-        const card_tr = document.createElement('tr');
-        for(var a = 0; a < 5; a++){
-            const card_td = document.createElement('td');
-            if(n == 2 && a == 2){
-                card_td.innerText = 'X';
-                card_tr.appendChild(card_td);
-            }else{
-                card_td.innerText = card[a][n]
-                card_tr.appendChild(card_td);
-            }
-        }
-        tbody_card.appendChild(card_tr);
+  th_B.innerText = 'B'
+  th_I.innerText = 'I'
+  th_N.innerText = 'N'
+  th_G.innerText = 'G'
+  th_O.innerText = 'O'
+
+
+  thead_card.appendChild(th_B);
+  thead_card.appendChild(th_I);
+  thead_card.appendChild(th_N);
+  thead_card.appendChild(th_G);
+  thead_card.appendChild(th_O);
+
+  for (var n = 0; n < 5; n++) {
+    const card_tr = document.createElement('tr');
+    for (var a = 0; a < 5; a++) {
+      const card_td = document.createElement('td');
+      card_td.innerText = player.card[a][n];
+      card_tr.appendChild(card_td);
     }
+    tbody_card.appendChild(card_tr);
+  }
 
 }
 
+// função para iniciar o jogo
 function startGame() {
-    if (totalPlayers.length < 2) {
-      alert("Você precisa ter pelo menos dois jogadores para iniciar o jogo!");
+  if (totalPlayers.length < 2) {
+    alert("Você precisa ter pelo menos dois jogadores para iniciar o jogo!");
+    return;
+  }
+
+  gameProgress = true;
+
+  const div_space_game = document.getElementById('space_game');
+  const drawnNumbers = [];
+  const intervalId = setInterval(() => {
+    if (drawnNumbers.length === 75) {
+      clearInterval(intervalId);
       return;
     }
-  
-    gameProgress = true;
-  
-    const div_space_game = document.getElementById('space_game');
-    const drawnNumbers = [];
-    const intervalIds = [];
-  
-    function checkCardComplete(card) {
-      for (let i = 0; i < card.length; i++) {
-        for (let j = 0; j < card[i].length; j++) {
-          if (card[i][j] !== 'X') {
-            return false;
-          }
-        }
-      }
-      return true;
-    }
-  
-    function drawRandomNumber(playerIndex) {
-      if (drawnNumbers.length === 75) {
-        clearInterval(intervalIds[playerIndex]);
-        return;
-      }
-  
-      let random_number;
-  
-      do {
-        random_number = Math.floor(Math.random() * 75 + 1);
-      } while (drawnNumbers.includes(random_number));
-  
-      drawnNumbers.push(random_number);
-  
-      const div_number = document.createElement('div');
-      div_number.className = 'number';
-      div_number.innerText = random_number;
-  
-      div_space_game.appendChild(div_number);
-  
-      // Marcar o número em cada cartela do jogador
-      const player = totalPlayers[playerIndex];
-      const { card } = player;
-  
-      for (let i = 0; i < card.length; i++) {
-        for (let j = 0; j < card[i].length; j++) {
-          if (card[i][j] === random_number) {
-            card[i][j] = 'X';
-          }
-        }
-      }
-  
-      // Verificar se a tabela do jogador está completa
-      if (checkCardComplete(card)) {
-        console.log(`O jogador ${player.name} completou a tabela!`);
-        // Aqui você pode adicionar a lógica para tratar o jogador vencedor como desejar
-      }
-    }
-  
-    totalPlayers.forEach((player, index) => {
-      const intervalId = setInterval(() => drawRandomNumber(index), 1000);
-      intervalIds.push(intervalId);
-    });
-  }
-  
+
+    let random;
+
+    do {
+      random = Math.floor(Math.random() * 75 + 1);
+    } while (drawnNumbers.includes(random));
+
+    drawnNumbers.push(random);
+
+    const div_number = document.createElement('div');
+    div_number.className = 'number';
+    div_number.innerText = random;
+
+    div_space_game.appendChild(div_number);
+  }, 1000);
+
+}
 
 
-function resetGame(){
-    location.reload();
+// função para reiniciar o jogo
+function resetGame() {
+  location.reload();
 }
 
 
